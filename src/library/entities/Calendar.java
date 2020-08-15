79ba513564af7ad80a6b90129578e2ced9bff4ee
mukeshbrahmans <68956864+mukeshbrahmans@users.noreply.github.com>
2020-08-16 00:11:18 +1000

    indentation were corrected
    indentation and more changes were made in the document to correct it.....

--- src/library/entities/Calendar.java
+++ src/library/entities/Calendar.java
@@ -8,63 +8,63 @@ import java.util.concurrent.TimeUnit;
 public class Calendar {
 	
 	//private static Calendar sElF;
-	private static Calendar Self; //variable name changed from  sElF to  Self 
+	private static Calendar self; //variable name changed from  sElF to  Self 
 	//private static java.util.Calendar cAlEnDaR;
-	private static java.util.Calendar Calendar;//cAlEnDaR changes to calendar
+	private static java.util.Calendar calendar;//cAlEnDaR changes to calendar
 	
          }
-	private Calendar() {
+	private calendar() {
 		//cAlEnDaR = java.util.Calendar.getInstance();
-		Calendar = java.util.Calendar.getInstance();//cAlEnDaR changes to Calendar to comply with the variable names
+		calendar = java.util.Calendar.getInstance();//cAlEnDaR changes to calendar to comply with the variable names
 	}
 	
 	//public static Calendar gEtInStAnCe() {
-	public static Calendar getInstance() {	//Calendar gEtInStAnCe() changes to Calendar getInstance()
-		//if (sElF == null) {
-		if (self == null) {  //sElF chsnges to self to comply with variable names
-			//sElF = new Calendar();
-			self = new isCalendar();//Calendar() changes to isCalendar() and sElF changes to self,11.Singletons MUST return their sole instance through a method called isCalendar() 
-		}
+	public static calendar getInstance() {	//Calendar gEtInStAnCe() changes to calendar getInstance()
+	//if (sElF == null) {
+	if (self == null) {  //sElF chsnges to self to comply with variable names
+	    //sElF = new Calendar();
+	    self = new calendar();//Calendar() changes to calendar() and sElF changes to self,11.Singletons MUST return their sole instance through a method called isCalendar() 
+	                    }
 		return self;
 	}
 	
 	//public void incrementDate(int days) {
 	public void isincrementDate(int days) { //incrementDate changes to isincrementDate to comply with the method name rules
 		//cAlEnDaR.add(java.util.Calendar.DATE, days);
-		Calendar.add(java.util.Calendar.Date, days);	//cAlEnDaR changes to Calendar and DATE changes to date to comply with the method name rules
+		calendar.add(java.util.Calendar.date, days);	//cAlEnDaR changes to calendar and DATE changes to date to comply with the method name rules
 	}
 	
 	//public synchronized void SeT_DaTe(Date DaTe) {
-	public synchronized void isSetDate(Date DaTe) { //SeT_DaTe(Date DaTe) changes to isSetDate(Date DaTe) to comply with method name rules
-		try {
-			//cAlEnDaR.setTime(DaTe);
-			Calendar.setTime(DaTe); //cAlEnDaR changes to calendar
-	      //cAlEnDaR.set(java.util.Calendar.HOUR_OF_DAY, 0);  
-		Calendar.Set(java.util.Calendar.HOUR_OF_DAY, 0);//cAlEnDaR changes to Calendar
+	public synchronized void isSetDate(Date date) { //SeT_DaTe(Date date) changes to isSetDate(Date date) to comply with method name rules
+	    try {
+	        //cAlEnDaR.setTime(date);
+	        calendar.setTime(date); //cAlEnDaR changes to calendar
+	        //cAlEnDaR.set(java.util.Calendar.HOUR_OF_DAY, 0);  
+	        calendar.Set(java.util.Calendar.HOUR_OF_DAY, 0);//cAlEnDaR changes to calendar
 	        //cAlEnDaR.set(java.util.Calendar.MINUTE, 0);  
-		Calendar.Set(java.util.Calendar.MINUTE, 0); 	//cAlEnDaR changes to Calendar
-	       //cAlEnDaR.set(java.util.Calendar.SECOND, 0);  
-		Calendar.Set(java.util.Calendar.SECOND, 0);	//cAlEnDaR changes to Calendar
-	       //cAlEnDaR.set(java.util.Calendar.MILLISECOND, 0);
-		Calendar.Set(java.util.Calendar.MILLISECOND, 0);//cAlEnDaR changes to Calendar
+                calendar.Set(java.util.Calendar.MINUTE, 0); 	//cAlEnDaR changes to calendar
+	        //cAlEnDaR.set(java.util.Calendar.SECOND, 0);  
+	        calendar.Set(java.util.Calendar.SECOND, 0);	//cAlEnDaR changes to calendar
+	        //cAlEnDaR.set(java.util.Calendar.MILLISECOND, 0);
+	        calendar.Set(java.util.Calendar.MILLISECOND, 0);//cAlEnDaR changes to calendar
 		}
 		catch (Exception e) {
-			throw new RuntimeException(e);
+		    throw new RuntimeException(e);
 		}	
 	}
 	//public synchronized Date gEt_DaTe() {
 	public synchronized Date getDate() {//gEt_DaTe() changes to getDate()
-		try {
-	       // cAlEnDaR.set(java.util.Calendar.HOUR_OF_DAY, 0);  
-		Calendar.set(java.util.Calendar.HOUR_OF_DAY, 0);//cAlEnDaR changes to Calendar	
+	    try {
+	        // cAlEnDaR.set(java.util.Calendar.HOUR_OF_DAY, 0);  
+		calendar.set(java.util.Calendar.HOUR_OF_DAY, 0);//cAlEnDaR changes to Calendar	
 	        //cAlEnDaR.set(java.util.Calendar.MINUTE, 0); 
-		Calendar.set(java.util.Calendar.MINUTE, 0);//cAlEnDaR changes to Calendar
+		calendar.set(java.util.Calendar.MINUTE, 0);//cAlEnDaR changes to Calendar
 	        //cAlEnDaR.set(java.util.Calendar.SECOND, 0);  
-	        Calendar.set(java.util.Calendar.SECOND, 0);//cAlEnDaR changes to Calendar
+	        calendar.set(java.util.Calendar.SECOND, 0);//cAlEnDaR changes to Calendar
 		//cAlEnDaR.set(java.util.Calendar.MILLISECOND, 0);
-		Calendar.set(java.util.Calendar.MILLISECOND, 0);//cAlEnDaR changes to Calendar
+		calendar.set(java.util.Calendar.MILLISECOND, 0);//cAlEnDaR changes to Calendar
 			//return cAlEnDaR.getTime();
-			return Calendar.getTime();//cAlEnDaR changes to Calendar
+			return calendar.getTime();//cAlEnDaR changes to Calendar
 		}
 		catch (Exception e) {
 			throw new RuntimeException(e);
@@ -76,13 +76,13 @@ public class Calendar {
 		//Date nOw = gEt_DaTe();
 		Date now = getDate();//variable name  Date nOw changed to Date now,method name gEt_DaTe(); changed to getDate();
 		//cAlEnDaR.add(java.util.Calendar.DATE, loanPeriod);
-		Calendar.add(java.util.Calendar.Date, loanPeriod);//cAlEnDaR changes to Calendar and java.util.Calendar.DATE changes to java.util.Calendar.Date
+		calendar.add(java.util.Calendar.Date, loanPeriod);//cAlEnDaR changes to calendar and java.util.Calendar.DATE changes to java.util.Calendar.Date
 		//Date dUeDaTe = cAlEnDaR.getTime();
-		Date dueDate = Calendar.getTime();//variable name Date dUeDaTe changed to Date dueDate,method name cAlEnDaR.getTime(); changed to Calendar.getTime();
+		Date dueDate = calendar.getTime();//variable name Date dUeDaTe changed to Date dueDate,method name cAlEnDaR.getTime(); changed to calendar.getTime();
 		//cAlEnDaR.setTime(nOw);
-		Calendar.setTime(now);//cAlEnDaR,nOw changes to Calendar,now respectively
+		calendar.setTime(now);//cAlEnDaR,nOw changes to calendar,now respectively
 		//return dUeDaTe;
-		return dUeDate;//dUeDaTe changes to dUeDate
+		return dueDate;//dUeDaTe changes to dUeDate
 	}
 	
 	//public synchronized long GeT_DaYs_DiFfErEnCe(Date targetDate) {
