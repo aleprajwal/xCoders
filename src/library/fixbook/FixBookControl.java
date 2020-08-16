bcff3cc255352cc26546c4046672e984b1d834be
unknown <sudhir.thapaliya@gmail.com>
2020-08-16 13:02:51 +1000

    Indentation fix

--- src/library/fixbook/FixBookControl.java
+++ src/library/fixbook/FixBookControl.java
@@ -10,116 +10,116 @@ import library.entities.Library;
 
 //public class fIX_bOOK_cONTROL {
 public class FixBookControl {// changed class name and renamed class file as well.
-	
-	//private FixBookUI Ui;
-	private FixBookUI ui;// changed variable name
-	//private enum CoNtRoL_StAtE { INITIALISED, READY, FIXING };
-	private enum ControlState { INITIALISED, READY, FIXING };// changed Enums name
-	//private CoNtRoL_StAtE StAtE;
-	private ControlState state;// changed enum variable name and Enums Type
-	
-	//private Library LiBrArY;
-	private Library library;// changed variable name
-	//private Book CuRrEnT_BoOk;
-	private Book currentBook;// changed variable name
+    
+    //private FixBookUI Ui;
+    private FixBookUI ui;// changed variable name
+    //private enum CoNtRoL_StAtE { INITIALISED, READY, FIXING };
+    private enum ControlState { INITIALISED, READY, FIXING };// changed Enums name
+    //private CoNtRoL_StAtE StAtE;
+    private ControlState state;// changed enum variable name and Enums Type
+    
+    //private Library LiBrArY;
+    private Library library;// changed variable name
+    //private Book CuRrEnT_BoOk;
+    private Book currentBook;// changed variable name
 
 
-	//public fIX_bOOK_cONTROL() {
-	public FixBookControl() {// changed constructors name
-		//this.LiBrArY = Library.GeTiNsTaNcE();
-		this.library = Library.GeTiNsTaNcE();// changed variable name only as the function is in Library class which is not refractored
-		//StAtE = CoNtRoL_StAtE.INITIALISED;
-		state = ControlState.INITIALISED;// changed variable name and enums name
-	}
-	
-	
-	//public void SeT_Ui(FixBookUI ui) {
-	public void setUI(FixBookUI ui) {// changed function name
-		//if (!StAtE.equals(CoNtRoL_StAtE.INITIALISED)) 
-			//throw new RuntimeException("FixBookControl: cannot call setUI except in INITIALISED state");
-		if (!state.equals(ControlState.INITIALISED)) {// changed variable names and added code block
+    //public fIX_bOOK_cONTROL() {
+    public FixBookControl() {// changed constructors name
+        //this.LiBrArY = Library.GeTiNsTaNcE();
+        this.library = Library.GeTiNsTaNcE();// changed variable name only as the function is in Library class which is not refractored
+        //StAtE = CoNtRoL_StAtE.INITIALISED;
+        state = ControlState.INITIALISED;// changed variable name and enums name
+    }
+    
+    
+    //public void SeT_Ui(FixBookUI ui) {
+    public void setUI(FixBookUI ui) {// changed function name
+        //if (!StAtE.equals(CoNtRoL_StAtE.INITIALISED)) 
+            //throw new RuntimeException("FixBookControl: cannot call setUI except in INITIALISED state");
+        if (!state.equals(ControlState.INITIALISED)) {// changed variable names and added code block
             throw new RuntimeException("FixBookControl: cannot call setUI except in INITIALISED state");
-		}
-			
-			
-		//this.Ui = ui;
-		this.ui = ui;// changed variable name
-		//ui.SeT_StAtE(FixBookUI.uI_sTaTe.READY);
-		ui.setState(FixBookUI.UIState.READY);// changed function call name
-		//StAtE = CoNtRoL_StAtE.READY;
-		state = ControlState.READY;		// changed enums reference and variable name
-	}
+        }
+            
+            
+        //this.Ui = ui;
+        this.ui = ui;// changed variable name
+        //ui.SeT_StAtE(FixBookUI.uI_sTaTe.READY);
+        ui.setState(FixBookUI.UIState.READY);// changed function call name
+        //StAtE = CoNtRoL_StAtE.READY;
+        state = ControlState.READY;        // changed enums reference and variable name
+    }
 
 
-	//public void BoOk_ScAnNeD(int BoOkId) {
+    //public void BoOk_ScAnNeD(int BoOkId) {
     public void bookScanned(int bookId) {// changed function and parameter name
-		// if (!StAtE.equals(CoNtRoL_StAtE.READY)) 
-		// 	throw new RuntimeException("FixBookControl: cannot call bookScanned except in READY state");
-		if (!state.equals(ControlState.READY)) {// changed variable name and added code block
+        // if (!StAtE.equals(CoNtRoL_StAtE.READY)) 
+        //     throw new RuntimeException("FixBookControl: cannot call bookScanned except in READY state");
+        if (!state.equals(ControlState.READY)) {// changed variable name and added code block
             throw new RuntimeException("FixBookControl: cannot call bookScanned except in READY state");
-		}
-			
-			
-		//CuRrEnT_BoOk = LiBrArY.gEt_BoOk(BoOkId);
-		currentBook = library.gEt_BoOk(BoOkId);// this function belongs to another class which is not refractored
-		
-		//if (CuRrEnT_BoOk == null) {
-		if (currentBook == null) {// changed variable
-			//Ui.dIsPlAy("Invalid bookId");
-			ui.display("Invalid bookId");// changed function name and variable name
-			return;
-		}
-		//if (!CuRrEnT_BoOk.iS_DaMaGeD()) {
-		if (!currentBook.isDamaged()) {// changed variable and function name
-			//Ui.dIsPlAy("Book has not been damaged");
-			ui.display("Book has not been damaged");// changed variable and function name
-			return;
-		}
-		//Ui.dIsPlAy(CuRrEnT_BoOk.toString());
-		ui.display(currentBook.toString());// changed variable and function name
-		//Ui.SeT_StAtE(FixBookUI.uI_sTaTe.FIXING);
-		ui.setState(FixBookUI.UIState.FIXING);// changed variable and function name
-		//StAtE = CoNtRoL_StAtE.FIXING;
-		state = ControlState.FIXING;	// changed variable and enum name		
-	}
+        }
+            
+            
+        //CuRrEnT_BoOk = LiBrArY.gEt_BoOk(BoOkId);
+        currentBook = library.gEt_BoOk(BoOkId);// this function belongs to another class which is not refractored
+        
+        //if (CuRrEnT_BoOk == null) {
+        if (currentBook == null) {// changed variable
+            //Ui.dIsPlAy("Invalid bookId");
+            ui.display("Invalid bookId");// changed function name and variable name
+            return;
+        }
+        //if (!CuRrEnT_BoOk.iS_DaMaGeD()) {
+        if (!currentBook.isDamaged()) {// changed variable and function name
+            //Ui.dIsPlAy("Book has not been damaged");
+            ui.display("Book has not been damaged");// changed variable and function name
+            return;
+        }
+        //Ui.dIsPlAy(CuRrEnT_BoOk.toString());
+        ui.display(currentBook.toString());// changed variable and function name
+        //Ui.SeT_StAtE(FixBookUI.uI_sTaTe.FIXING);
+        ui.setState(FixBookUI.UIState.FIXING);// changed variable and function name
+        //StAtE = CoNtRoL_StAtE.FIXING;
+        state = ControlState.FIXING;    // changed variable and enum name        
+    }
 
 
-	//public void FiX_BoOk(boolean mUsT_FiX) {
-	public void fixBook(boolean mustFix) {// change function and parametername
-		// if (!StAtE.equals(CoNtRoL_StAtE.FIXING)) 
-		// 	throw new RuntimeException("FixBookControl: cannot call fixBook except in FIXING state");
+    //public void FiX_BoOk(boolean mUsT_FiX) {
+    public void fixBook(boolean mustFix) {// change function and parametername
+        // if (!StAtE.equals(CoNtRoL_StAtE.FIXING)) 
+        //     throw new RuntimeException("FixBookControl: cannot call fixBook except in FIXING state");
 
-		if (!state.equals(ControlState.FIXING)) { // changed variables name and added code block
+        if (!state.equals(ControlState.FIXING)) { // changed variables name and added code block
             throw new RuntimeException("FixBookControl: cannot call fixBook except in FIXING state");
-		}
-			
-			
-		// if (mUsT_FiX) 
-		// 	LiBrArY.RePaIr_BoOk(CuRrEnT_BoOk);
-		if (mustFix) {// changed variable name and added code block
-			library.RePaIr_BoOk(currentBook);//this function belongs to libray class
-		}
-			
-		
-		//CuRrEnT_BoOk = null;
-		currentBook = null;// changed variable name
-		//Ui.SeT_StAtE(FixBookUI.uI_sTaTe.READY);
-		ui.setState(FixBookUI.UIState.READY);// changed variable name and function name
-		//StAtE = CoNtRoL_StAtE.READY;
-		state = ControlState.READY;	// changed variable name and enum reference	
-	}
+        }
+            
+            
+        // if (mUsT_FiX) 
+        //     LiBrArY.RePaIr_BoOk(CuRrEnT_BoOk);
+        if (mustFix) {// changed variable name and added code block
+            library.RePaIr_BoOk(currentBook);//this function belongs to libray class
+        }
+            
+        
+        //CuRrEnT_BoOk = null;
+        currentBook = null;// changed variable name
+        //Ui.SeT_StAtE(FixBookUI.uI_sTaTe.READY);
+        ui.setState(FixBookUI.UIState.READY);// changed variable name and function name
+        //StAtE = CoNtRoL_StAtE.READY;
+        state = ControlState.READY;    // changed variable name and enum reference    
+    }
 
-	
-	//public void SCannING_COMplete() {
-	public void scanningComplete() {// changed function name
-		// if (!StAtE.equals(CoNtRoL_StAtE.READY)) 
-		// 	throw new RuntimeException("FixBookControl: cannot call scanningComplete except in READY state");
-		if(!state.equals(ControlState.READY)) {// changed variable name and added code block
-			throw new RuntimeException("FixBookControl: cannot call scanningComplete except in READY state");
-		}
-			
-		//Ui.SeT_StAtE(FixBookUI.uI_sTaTe.COMPLETED);
-		ui.setState(FixBookUI.UIState.COMPLETED);// changed variable and enum reference		
-	}
+    
+    //public void SCannING_COMplete() {
+    public void scanningComplete() {// changed function name
+        // if (!StAtE.equals(CoNtRoL_StAtE.READY)) 
+        //     throw new RuntimeException("FixBookControl: cannot call scanningComplete except in READY state");
+        if(!state.equals(ControlState.READY)) {// changed variable name and added code block
+            throw new RuntimeException("FixBookControl: cannot call scanningComplete except in READY state");
+        }
+            
+        //Ui.SeT_StAtE(FixBookUI.uI_sTaTe.COMPLETED);
+        ui.setState(FixBookUI.UIState.COMPLETED);// changed variable and enum reference        
+    }
 
 }
--- src/library/fixbook/FixBookUI.java
+++ src/library/fixbook/FixBookUI.java
@@ -7,119 +7,119 @@ import java.util.Scanner;
 
 public class FixBookUI {
 
-	//public static enum uI_sTaTe { INITIALISED, READY, FIXING, COMPLETED };
-	public static enum UIState { INITIALISED, READY, FIXING, COMPLETED };//change enum naming convention
-	//private fIX_bOOK_cONTROL CoNtRoL;
-	private FixBookControl control;//changed reference of Class as the class name is change i fix book control
-	//private Scanner InPuT;
-	private Scanner input;// fixed variable naming convention
-	//private uI_sTaTe StAtE;
-	private UIState state;// fixed variable naming convention
+    //public static enum uI_sTaTe { INITIALISED, READY, FIXING, COMPLETED };
+    public static enum UIState { INITIALISED, READY, FIXING, COMPLETED };//change enum naming convention
+    //private fIX_bOOK_cONTROL CoNtRoL;
+    private FixBookControl control;//changed reference of Class as the class name is change i fix book control
+    //private Scanner InPuT;
+    private Scanner input;// fixed variable naming convention
+    //private uI_sTaTe StAtE;
+    private UIState state;// fixed variable naming convention
 
-	
-	//public FixBookUI(fIX_bOOK_cONTROL CoNtRoL) {
-	public FixBookUI(FixBookControl control) {// fixed Parameter Class name and argument naming convention
-		//this.CoNtRoL = CoNtRoL;
-		this.control = control;// variable naming convention fix
-		//InPuT = new Scanner(System.in);
-		input = new Scanner(System.in);// variable naming convention fix
-		//StAtE = uI_sTaTe.INITIALISED;
-		state = UIState.INITIALISED;// Enum/variable naming convention fix
-		//CoNtRoL.SeT_Ui(this);
-		control.setUI(this);// changed the varianble and function name as he function is changed in fixbookcontrol.java
-	}
+    
+    //public FixBookUI(fIX_bOOK_cONTROL CoNtRoL) {
+    public FixBookUI(FixBookControl control) {// fixed Parameter Class name and argument naming convention
+        //this.CoNtRoL = CoNtRoL;
+        this.control = control;// variable naming convention fix
+        //InPuT = new Scanner(System.in);
+        input = new Scanner(System.in);// variable naming convention fix
+        //StAtE = uI_sTaTe.INITIALISED;
+        state = UIState.INITIALISED;// Enum/variable naming convention fix
+        //CoNtRoL.SeT_Ui(this);
+        control.setUI(this);// changed the varianble and function name as he function is changed in fixbookcontrol.java
+    }
 
 
-	//public void SeT_StAtE(uI_sTaTe state) {
-	public void setState(UIState state) {//fixed method and argument naming convention
-		//this.StAtE = state;
-		this.state = state;// fixed variables naming convention
-	}
+    //public void SeT_StAtE(uI_sTaTe state) {
+    public void setState(UIState state) {//fixed method and argument naming convention
+        //this.StAtE = state;
+        this.state = state;// fixed variables naming convention
+    }
 
-	//public void RuN() {
-	public void run() {//change methods naming convvention
-		//OuTpUt("Fix Book Use Case UI\n");
-		output("Fix Book Use Case UI\n");// changed functions name which is inside this class
-		
-		while (true) {
-			
-			//switch (StAtE) {
-			switch (state) {// changed variable name
+    //public void RuN() {
+    public void run() {//change methods naming convvention
+        //OuTpUt("Fix Book Use Case UI\n");
+        output("Fix Book Use Case UI\n");// changed functions name which is inside this class
+        
+        while (true) {
+            
+            //switch (StAtE) {
+            switch (state) {// changed variable name
 
-			case READY:
-				//String BoOk_EnTrY_StRiNg = iNpUt("Scan Book (<enter> completes): ");
-				String bookEntryString = input("Scan Book (<enter> completes): ");// changed variable name
-				//if (BoOk_EnTrY_StRiNg.length() == 0) 
-				if(bookEntryString.length() == 0) {// added code block and changed variable naming conventions
-					//CoNtRoL.SCannING_COMplete();
-					control.scanningComplete();
-				}
-				
-				else {
-					try {
-						//int BoOk_Id = Integer.valueOf(BoOk_EnTrY_StRiNg).intValue();
-						int bookId = Integer.valueOf(bookEntryString).intValue();// changed variable naming conventions
-						//CoNtRoL.BoOk_ScAnNeD(BoOk_Id);
-						control.bookScanned(bookId);// changed function and variable name. Function name has been changed in Fixbookcontrol.java as well.
-					}
-					catch (NumberFormatException e) {
-						//OuTpUt("Invalid bookId");
-						output("Invalid bookId");// changed function name
-					}
-				}
-				break;	
-				
-			case FIXING:
-				//String AnS = iNpUt("Fix Book? (Y/N) : ");
-				String answer = input("Fix Book? (Y/N) : ");// changed variable /function name
-				//boolean FiX = false;
-				boolean fix = false;// changed variable name
-				//if (AnS.toUpperCase().equals("Y"))
-				    //FiX = true;
-				if (answer.toUpperCase().equals("Y")) {//changed variable name and also added code block
+            case READY:
+                //String BoOk_EnTrY_StRiNg = iNpUt("Scan Book (<enter> completes): ");
+                String bookEntryString = input("Scan Book (<enter> completes): ");// changed variable name
+                //if (BoOk_EnTrY_StRiNg.length() == 0) 
+                if(bookEntryString.length() == 0) {// added code block and changed variable naming conventions
+                    //CoNtRoL.SCannING_COMplete();
+                    control.scanningComplete();
+                }
+                
+                else {
+                    try {
+                        //int BoOk_Id = Integer.valueOf(BoOk_EnTrY_StRiNg).intValue();
+                        int bookId = Integer.valueOf(bookEntryString).intValue();// changed variable naming conventions
+                        //CoNtRoL.BoOk_ScAnNeD(BoOk_Id);
+                        control.bookScanned(bookId);// changed function and variable name. Function name has been changed in Fixbookcontrol.java as well.
+                    }
+                    catch (NumberFormatException e) {
+                        //OuTpUt("Invalid bookId");
+                        output("Invalid bookId");// changed function name
+                    }
+                }
+                break;    
+                
+            case FIXING:
+                //String AnS = iNpUt("Fix Book? (Y/N) : ");
+                String answer = input("Fix Book? (Y/N) : ");// changed variable /function name
+                //boolean FiX = false;
+                boolean fix = false;// changed variable name
+                //if (AnS.toUpperCase().equals("Y"))
+                    //FiX = true;
+                if (answer.toUpperCase().equals("Y")) {//changed variable name and also added code block
                     fix = true;
-				}
-					
-				
-				//CoNtRoL.FiX_BoOk(FiX);
-				control.fixbook(fix);// changed variable name and function name. Function name is changed in it definition as well.
-				break;
-								
-			case COMPLETED:
-				//OuTpUt("Fixing process complete");
-				output("Fixing process complete");// changed function name
-				return;
-			
-			default:
-				//OuTpUt("Unhandled state");
-				output("Unhandled state");// changed function name
-				throw new RuntimeException("FixBookUI : unhandled state :" + state);			
-			
-			}		
-		}
-		
-	}
+                }
+                    
+                
+                //CoNtRoL.FiX_BoOk(FiX);
+                control.fixbook(fix);// changed variable name and function name. Function name is changed in it definition as well.
+                break;
+                                
+            case COMPLETED:
+                //OuTpUt("Fixing process complete");
+                output("Fixing process complete");// changed function name
+                return;
+            
+            default:
+                //OuTpUt("Unhandled state");
+                output("Unhandled state");// changed function name
+                throw new RuntimeException("FixBookUI : unhandled state :" + state);            
+            
+            }        
+        }
+        
+    }
 
-	
-	//private String iNpUt(String prompt) {
-	private String input(String prompt) {// changed function name
-		System.out.print(prompt);
-		//return InPuT.nextLine();
-		return input.nextLine();// change variable name
-	}	
-		
-		
-	//private void OuTpUt(Object object) {
-	private void output(Object object) {// changed function name
-		System.out.println(object);
-	}
-	
+    
+    //private String iNpUt(String prompt) {
+    private String input(String prompt) {// changed function name
+        System.out.print(prompt);
+        //return InPuT.nextLine();
+        return input.nextLine();// change variable name
+    }    
+        
+        
+    //private void OuTpUt(Object object) {
+    private void output(Object object) {// changed function name
+        System.out.println(object);
+    }
+    
 
-	//public void dIsPlAy(Object object) {
-	public void display(Object object) {// changed function name
-		//OuTpUt(object);
-		output(object);// changed function name
-	}
-	
-	
+    //public void dIsPlAy(Object object) {
+    public void display(Object object) {// changed function name
+        //OuTpUt(object);
+        output(object);// changed function name
+    }
+    
+    
 }
