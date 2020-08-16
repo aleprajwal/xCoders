2aba39d9907d74ac2f69534d4a66ee52500d3a81
aleprajwal <aleprajwal@gmail.com>
2020-08-17 00:29:35 +1000

    static review on method swiped() according to the guideline

--- src/library/borrowbook/bORROW_bOOK_cONTROL.java
+++ src/library/borrowbook/bORROW_bOOK_cONTROL.java
@@ -34,7 +34,8 @@ public class BorrowBookControl { //changed class name bORROW_bOOK_cONTROL to Bor
 	private Book book; // changed variable name bOoK to book
 	
 	
-	public bORROW_bOOK_cONTROL() {
+	// public bORROW_bOOK_cONTROL() {
+	public BorrowBookControl() { // changed bORROW_bOOK_cONTROL() to BorrowBookControl()
 //		this.lIbRaRy = Library.GeTiNsTaNcE();
 		this.library = Library.GeTiNsTaNcE();
 		// sTaTe = CONTROL_STATE.INITIALISED;
@@ -42,37 +43,49 @@ public class BorrowBookControl { //changed class name bORROW_bOOK_cONTROL to Bor
 	}
 	
 
-	public void SeT_Ui(BorrowBookUI Ui) {
+	// public void SeT_Ui(BorrowBookUI Ui) {
+		public void setUI(BorrowBookUI ui) { // changed method name SeT_Ui to setUI and parameter Ui to ui
 		// if (!sTaTe.equals(CONTROL_STATE.INITIALISED))
 		if (!state.equals(CONTROL_STATE.INITIALISED)) { //changed sTaTe to state and added {} to if statement  
 			throw new RuntimeException("BorrowBookControl: cannot call setUI except in INITIALISED state");
 	    }	
-		this.uI = Ui;
-		Ui.SeT_StAtE(BorrowBookUI.uI_STaTe.READY);
+		// this.uI = Ui;
+		this.ui = ui; //changed variabe uI to ui and Ui to ui 
+//		Ui.SeT_StAtE(BorrowBookUI.uI_STaTe.READY);
+	    ui.setState(BorrowBookUI.UIState.READY); // changed variable Ui to ui; SeT_StAtE to setState and uI_STaTe to UIState[BorrowBookUI.java]
 		// sTaTe = CONTROL_STATE.READY;
 		state = CONTROL_STATE.READY; // changed sTaTe to state		
 	}
 
 		
-	public void SwIpEd(int mEmBeR_Id) {
+//	public void SwIpEd(int mEmBeR_Id) {
+    public void swiped(int memberID) { // changed method name SwIpEd to swiped and variable name mEmBeR_Id to memberID
 		// if (!sTaTe.equals(CONTROL_STATE.READY))
 		if (!state.equals(CONTROL_STATE.READY)) { // changed sTaTe to state and added {} to if statement
 			throw new RuntimeException("BorrowBookControl: cannot call cardSwiped except in READY state");
 		}
-		mEmBeR = lIbRaRy.gEt_MeMbEr(mEmBeR_Id);
-		if (mEmBeR == null) {
+//		mEmBeR = lIbRaRy.gEt_MeMbEr(mEmBeR_Id);
+		member = library.gEt_MeMbEr(memberID); //changed variable mEmBeR to member, lIbRaRy to library and mEmBeR_Id to memberID
+//		if (mEmBeR == null) {
+		if (member == null) { // changed mEmBeR to member
 			// uI.DiSpLaY("Invalid memberId");
-			ui.DiSpLaY("Invalid memberId"); // changed uI to ui
+			ui.display("Invalid memberId"); // changed uI to ui; DiSpLaY to display[BorrowBookUI.java]
 			return;
 		}
-		if (lIbRaRy.cAn_MeMbEr_BoRrOw(mEmBeR)) {
-			pEnDiNg_LiSt = new ArrayList<>();
-			uI.SeT_StAtE(BorrowBookUI.uI_STaTe.SCANNING);
-			sTaTe = CONTROL_STATE.SCANNING; 
+//		if (lIbRaRy.cAn_MeMbEr_BoRrOw(mEmBeR)) {
+		if (library.cAn_MeMbEr_BoRrOw(member)) { // changed lIbRaRy to library and mEmBeR to member
+//			pEnDiNg_LiSt = new ArrayList<>();
+			pendingList = new ArrayList<>(); // changed array name pEnDiNg_LiSt to pendingList
+			// uI.SeT_StAtE(BorrowBookUI.uI_STaTe.SCANNING);
+			ui.setState(BorrowBookUI.UIState.SCANNING); // method SeT_StAtE to setState and enum uI_STaTe to UIState [BorrowBookUI.java]
+			// sTaTe = CONTROL_STATE.SCANNING;
+			state = ControlState.SCANNING; //changed sTaTe to state and CONTROL_STATE to ControlState
 		}
 		else {
-			uI.DiSpLaY("Member cannot borrow at this time");
-			uI.SeT_StAtE(BorrowBookUI.uI_STaTe.RESTRICTED); 
+			// uI.DiSpLaY("Member cannot borrow at this time");
+			ui.display("Member cannot borrow at this time"); // method DiSpLaY to display[BorrowBookUI.java]
+			// uI.SeT_StAtE(BorrowBookUI.uI_STaTe.RESTRICTED);
+			ui.setState(BorrowBookUI.UIState.RESTRICTED); // changed method SeT_StAtE to setState and enum variable uI_STaTe to UIState[BorrowBookUI.java]
 		}
 	}
 	
